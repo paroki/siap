207cdf197e77b80611d3a889e1eda937bf3cbf2f
Anthonius Munthi <me@itstoni.com>
2019-05-10 21:41:56 +0800

    test

--- etc/travis/deploy.sh
+++ etc/travis/deploy.sh
@@ -6,6 +6,7 @@ ssh -i ./deploy_key travis@$IP -p $PORT <<EOF
   composer install --ansi
   yarn install
   yarn build
+  ./bin/console doctrine:query:sql 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
   ./bin/console doctrine:database:create --if-not-exists
   ./bin/console doctrine:schema:update --force
   ./bin/console cache:clear
