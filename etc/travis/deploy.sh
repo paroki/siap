be76eb76b55bbe475a4f0ba8ee0a07ffe2d8b069
Anthonius Munthi <me@itstoni.com>
2019-05-10 20:45:32 +0800

    test

--- etc/travis/deploy.sh
+++ etc/travis/deploy.sh
@@ -1,10 +1,14 @@
-ssh -i ./deploy_key travis@itstoni.com
-echo $PWD
-git pull origin master
-composer install
-yarn install
-yarn build
-./bin/console doctrine:schema:update --force
-./bin/console cache:clear
+git config --global push.default matching
+git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git push deploy master
 
+ssh -i ./deploy_key travis@itstoni.com
+ssh apps@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  composer install
+  yarn install
+  yarn build
+  ./bin/console doctrine:schema:update --force
+  ./bin/console cache:clear
+EOF
 exit 0
